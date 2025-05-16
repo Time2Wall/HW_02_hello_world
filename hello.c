915cce7c212d946d50a0333873684496467cb904
Denys Bez <denysbez.fi@gmail.com>
2025-05-16 09:24:02 +0300

    clang update

--- hello.c
+++ hello.c
@@ -7,12 +7,16 @@ MODULE_AUTHOR("Denys");
 MODULE_DESCRIPTION("Hello World Linux kernel module");
 MODULE_VERSION("1.0");
 
-static int __init hello_init(void) {
-    printk(KERN_INFO "Hello, world!\n");
-    return 0;
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello, world!\n");
+	return 0;
 }
 
-static void __exit hello_exit(void) { printk(KERN_INFO "Goodbye!\n"); }
+static void __exit hello_exit(void)
+{
+	printk(KERN_INFO "Goodbye!\n");
+}
 
 module_init(hello_init);
 module_exit(hello_exit);
